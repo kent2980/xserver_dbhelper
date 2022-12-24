a31337f3145d7e4e3185ddbcc75a179a2e0882aa
kent <kent2980@gmail.com>
2022-12-24 13:37:04 +0900

    commit

--- xserver_dbhelper/xserver_db_connect.py
+++ xserver_dbhelper/xserver_db_connect.py
@@ -135,6 +135,7 @@ class DBHelper:
             self.db_user = json_data[u"db_user"]
             self.db_password = json_data[u"db_password"]
             self.ssh_pkey = os.path.join(db_setting_json_path, json_data[u"ssh_pkey_name"])
+            print(self.ssh_pkey)
             self.ssh_host = json_data[u"ssh_host"]
             self.ssh_port = int(json_data[u"ssh_port"])
             self.ssh_user = json_data[u"ssh_user"]
